57a7c089e792f187dfe5cb3946366ce814a470e9
Victor Santiago <vitongos@users.noreply.github.com>
2019-06-20 21:03:41 +0200

    Create docker.sh

---
+++ deploy/docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum-config-manager --enable docker-ce-edge
+yum-config-manager --enable docker-ce-testing
+yum-config-manager --disable docker-ce-edge
+yum makecache fast
+yum install -y docker-ce.x86_64
+systemctl enable docker
+systemctl start docker
+usermod -a -G docker centos
