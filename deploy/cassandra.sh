2cc67e0de3703637de218723392b6793fbb3f505
Victor Santiago <vitongos@users.noreply.github.com>
2019-06-21 13:02:37 +0200

    Update cassandra.sh

--- deploy/cassandra.sh
+++ deploy/cassandra.sh
@@ -4,4 +4,4 @@ docker run -d -v /home/centos/cassandra-src:/cassandra --name cassandra1 cassand
 
 sleep 20
 
-docker run -d -e CASSANDRA_SEEDS="$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' cassandra1)"  -v /home/centos/cassandra-src:/nosql --name cassandra2 cassandra:latest
+docker run -d -e CASSANDRA_SEEDS="$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' cassandra1)"  -v /home/centos/cassandra-src:/cassandra --name cassandra2 cassandra:latest
